c67b4ada05d04cad7f4798a6ddf9913267829d3f
ange1020 <angelina.30571079@uru.edu>
2025-07-04 15:30:28 -0400

    feat: Aqui funciona el build

--- app.config.js
+++ app.config.js
@@ -4,19 +4,23 @@ export default {
     slug: "ReviewsNative",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/images/icon.png",
+    icon: "./assets/images/peliculas-tv.jpeg",
     scheme: "reviewsnative",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
     extra: {
-      EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000/api"
+      EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000/api",
+      "eas": {
+        "projectId": "9ef771a0-dda4-4015-bd0a-1cf89ae0a8fa"
+      }
     },
     ios: {
       supportsTablet: true
     },
     android: {
+      package: "com.ange1024.ReviewsNative",
       adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
+        foregroundImage: "./assets/images/peliculas-tv.jpeg",
         backgroundColor: "#ffffff"
       },
       edgeToEdgeEnabled: true
@@ -31,7 +35,7 @@ export default {
       [
         "expo-splash-screen",
         {
-          image: "./assets/images/splash-icon.png",
+          image: "./assets/images/peliculas-tv.jpeg",
           imageWidth: 200,
           resizeMode: "contain",
           backgroundColor: "#ffffff"
